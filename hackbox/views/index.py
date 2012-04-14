34255458f43424071fbea89ec59da1ae633b1120
Huan Do <doboy0@gmail.com>
2012-04-14 16:34:40 -0700

    made able to save in session

--- hackbox/views/index.py
+++ hackbox/views/index.py
@@ -1,5 +1,5 @@
-from flask import redirect, render_template
-from dropbox import client, session
+from flask import redirect, render_template, session
+import dropbox
 from hackbox import app
 
 
@@ -7,18 +7,21 @@ from hackbox import app
 def index():
     return render_template('index.jade')
 
-sess = session.DropboxSession(app.config['APP_KEY'], 
-                              app.config['APP_SECRET'], 
-                              app.config['ACCESS_TYPE'])
+sess = dropbox.session.DropboxSession(app.config['APP_KEY'], 
+                                      app.config['APP_SECRET'], 
+                                      app.config['ACCESS_TYPE'])
 
 @app.route('/login')
 def login():
     request_token = sess.obtain_request_token()
-    url = sess.build_authorize_url(request_token, oauth_callback="http://localhost:5000/login/%s/%s" % (request_token.key, request_token.secret))
+    url = sess.build_authorize_url(request_token, oauth_callback="http://localhost:5000/")
+    session['request_token'] = request_token
     return redirect(url)
 
-@app.route('/login/<request_key>/<request_secret>')
-def auth_login(request_key, request_secret ):
-    request_token = sess.set_request_token(request_key, request_secret )
-    access_token = sess.obtain_access_token(request_token)
+@app.route('/')
+def index():
+    request_token = session['request_token']
+    access_token = session['access_token'] = sess.obtain_access_token(request_token)
     return render_template('hello.html', access_token=access_token)
+
+
