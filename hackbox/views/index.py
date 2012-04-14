604bed1b140b41a63925968c97fb60b8e2738f56
Huan Do <doboy0@gmail.com>
2012-04-14 16:08:54 -0700

    basic auhtentication

--- hackbox/views/index.py
+++ hackbox/views/index.py
@@ -1,6 +1,24 @@
+from flask import redirect, render_template
+from dropbox import client, session
 from hackbox import app
-from flask import render_template
+
 
 @app.route('/')
 def index():
     return render_template('index.jade')
+
+sess = session.DropboxSession(app.config['APP_KEY'], 
+                              app.config['APP_SECRET'], 
+                              app.config['ACCESS_TYPE'])
+
+@app.route('/login')
+def login():
+    request_token = sess.obtain_request_token()
+    url = sess.build_authorize_url(request_token, oauth_callback="http://localhost:5000/login/%s/%s" % (request_token.key, request_token.secret))
+    return redirect(url)
+
+@app.route('/login/<request_key>/<request_secret>')
+def auth_login(request_key, request_secret ):
+    request_token = sess.set_request_token(request_key, request_secret )
+    access_token = sess.obtain_access_token(request_token)
+    return render_template('hello.html', access_token=access_token)
