4b18f634a1838bf74ce868262cd40bf6e251206f
Rocky Duan <dementrock@gmail.com>
2012-04-14 16:14:55 -0700

    use url_for instead of explicit url

--- hackbox/views/index.py
+++ hackbox/views/index.py
@@ -1,4 +1,4 @@
-from flask import redirect, render_template
+from flask import redirect, render_template, url_for
 from dropbox import client, session
 from hackbox import app
 
@@ -11,13 +11,13 @@ sess = session.DropboxSession(app.config['APP_KEY'],
                               app.config['APP_SECRET'], 
                               app.config['ACCESS_TYPE'])
 
-@app.route('/login')
+@app.route('/login/')
 def login():
     request_token = sess.obtain_request_token()
-    url = sess.build_authorize_url(request_token, oauth_callback="http://localhost:5000/login/%s/%s" % (request_token.key, request_token.secret))
+    url = sess.build_authorize_url(request_token, oauth_callback=url_for('auth_login', request_key=request_token.key, request_secret=request_token.secret, _external=True))
     return redirect(url)
 
-@app.route('/login/<request_key>/<request_secret>')
+@app.route('/login/<request_key>/<request_secret>/')
 def auth_login(request_key, request_secret ):
     request_token = sess.set_request_token(request_key, request_secret )
     access_token = sess.obtain_access_token(request_token)
