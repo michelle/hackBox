416c27c50d023bcba1ba8739884ba5dde61a2e46
Michelle Bu <analogmidnight@gmail.com>
2012-04-14 20:26:30 -0700

    working scroll

--- hackbox/static/foo.js
+++ hackbox/static/foo.js
@@ -1,25 +1,30 @@
 $(document).ready(function() {
     currentPosition = 0;
-    $("#sidebar").mousemove(function(e){
+    $("#sidebar").mousemove(function(e) {
         var h = $(this).height();
         var bottom = $("#height").height();
+
         var offset = $(this).offset();
-        var position = (e.pageY-offset.top)/h;
-        
-        if(position<0.05) {
-            currentPosition -= 5;
+        var position = (e.pageY - offset.top) / h;
+
+        if (position < 0.03) {
+            currentPosition -= 10;
             currentPosition = currentPosition < 0 ? 0 : currentPosition;
-            $(this).stop().animate({ scrollTop: currentPosition }, 500);
+
+            $(this).stop().animate({ scrollTop: currentPosition }, 500, "linear");
+
         }
-        if(position>0.95) {
-            currentPosition += 5;
+
+        if (position > 0.97) {
+            currentPosition += 10;
             currentPosition = currentPosition > bottom ? bottom : currentPosition;
-            $(this).stop().animate({ scrollTop: currentPosition }, 500);
+            $(this).stop().animate({ scrollTop: currentPosition }, 500, "linear");
+
         }
     });
-    
-    $('#sidebar').mouseleave(function(e) {
+
+    $("#sidebar").mouseleave(function(e) {
         $(this).stop();
     });
 
-});​
+});
