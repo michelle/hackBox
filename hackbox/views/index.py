61884c5db7ec0d8e4a308f06656533ae7125ae19
Huan Do <doboy0@gmail.com>
2012-04-14 17:36:10 -0700

    change views, so it handles more than one user

--- hackbox/views/index.py
+++ hackbox/views/index.py
@@ -1,27 +1,26 @@
-from flask import redirect, render_template, session
+from flask import redirect, render_template, session, request
 import dropbox
 from hackbox import app
 
-
-@app.route('/')
-def index():
-    return render_template('index.jade')
-
-sess = dropbox.session.DropboxSession(app.config['APP_KEY'], 
-                                      app.config['APP_SECRET'], 
-                                      app.config['ACCESS_TYPE'])
-
-@app.route('/login')
+@app.route('/login/')
 def login():
-    request_token = sess.obtain_request_token()
+    session['sess'] = sess = dropbox.session.DropboxSession(app.config['APP_KEY'], 
+                                                            app.config['APP_SECRET'], 
+                                                            app.config['ACCESS_TYPE'])
+    session['request_token'] = request_token = sess.obtain_request_token()
     url = sess.build_authorize_url(request_token, oauth_callback="http://localhost:5000/")
-    session['request_token'] = request_token
     return redirect(url)
 
 @app.route('/')
 def index():
-    request_token = session['request_token']
-    access_token = session['access_token'] = sess.obtain_access_token(request_token)
-    return render_template('hello.html', access_token=access_token)
-
-
+    try:
+        session['sess'].obtain_access_token(session['request_token'])
+        client = session['client'] = dropbox.client.DropboxClient(session['sess'])
+        return render_template('hello.html', access_token="None")
+    except KeyError as e:
+        print e
+        redirect('/login/')
+        
+@app.route('/get_folder_data')
+def get_folder_data():
+    return {}
