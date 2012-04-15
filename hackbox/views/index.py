b856023abc20da51786fb3c72960a9a752107532
Rocky Duan <dementrock@gmail.com>
2012-04-14 19:26:21 -0700

    decorator for auth & get_folder view

--- hackbox/views/index.py
+++ hackbox/views/index.py
@@ -1,6 +1,7 @@
-from flask import redirect, render_template, session, request
+from flask import redirect, render_template, session, request, jsonify
 import dropbox
 from hackbox import app
+from helper import nested_list, with_folder_size
 
 @app.route('/login/')
 def login():
@@ -11,16 +12,19 @@ def login():
     url = sess.build_authorize_url(request_token, oauth_callback="http://localhost:5000/")
     return redirect(url)
 
+@dropbox_auth_required
 @app.route('/')
 def index():
-    try:
-        session['sess'].obtain_access_token(session['request_token'])
-        client = session['client'] = dropbox.client.DropboxClient(session['sess'])
-        return render_template('hello.html', access_token="None")
-    except KeyError as e:
-        print e
-        return redirect('/login/')
+    session['sess'].obtain_access_token(session['request_token'])
+    client = session['client'] = dropbox.client.DropboxClient(session['sess'])
+    return render_template('hello.html', access_token="None")
         
+@dropbox_auth_required
 @app.route('/get_folder_data')
 def get_folder_data():
-    return {}
+    client = session['clinet']
+    if 'folder_data' not in session['folder_data']:
+        folder_data = nested_list(with_folder_size(client.delta()))
+    else:
+        folder_data = session['folder_data']
+    return jsonify(nested_list(with_folder_size(client.delta())))
