23eacb038baf5c302669b80c43253d41dd504d5f
Michelle Bu <analogmidnight@gmail.com>
2012-04-14 20:11:44 -0700

    NOT WORKING

--- hackbox/static/foo.js
+++ hackbox/static/foo.js
@@ -1,1 +1,25 @@
-console.log(3);
+$(document).ready(function() {
+    currentPosition = 0;
+    $("#sidebar").mousemove(function(e){
+        var h = $(this).height();
+        var bottom = $("#height").height();
+        var offset = $(this).offset();
+        var position = (e.pageY-offset.top)/h;
+        
+        if(position<0.05) {
+            currentPosition -= 5;
+            currentPosition = currentPosition < 0 ? 0 : currentPosition;
+            $(this).stop().animate({ scrollTop: currentPosition }, 500);
+        }
+        if(position>0.95) {
+            currentPosition += 5;
+            currentPosition = currentPosition > bottom ? bottom : currentPosition;
+            $(this).stop().animate({ scrollTop: currentPosition }, 500);
+        }
+    });
+    
+    $('#sidebar').mouseleave(function(e) {
+        $(this).stop();
+    });
+
+});​
