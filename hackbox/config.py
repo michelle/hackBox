a150f8e6b7555e0788ae8236a9ec9e2203c33d71
Huan Do <doboy0@gmail.com>
2012-04-16 17:08:16 -0700

    change config for heroku

--- hackbox/config.py
+++ hackbox/config.py
@@ -1,10 +1,12 @@
 DEBUG = True
 SECRET_KEY = "DOBOY"
 
-MONGODB_HOST = "127.0.0.1"
+#mongo
+MONGODB_HOST = "127.0.0.1" if DEBUG else "mongodb://heroku_app4029146:o5k8eqsticr0isk45dbe5tqdg@ds031857.mongolab.com:31857/heroku_app4029146"
 MONGODB_PORT = 27017
 MONGODB_DATABASE = "hackbox"
 
+#dropbox
 APP_KEY = "ro9f4awbcf19pb0"
 APP_SECRET = "rfr4xvnad9nxtnb"
 ACCESS_TYPE = "dropbox"
